19fb79ae87ae99450b06ece39462af37647a2809
Gajendra Jung Katuwal <himalaya.jung@gmail.com>
2015-12-27 18:40:27 -0500

    heliotropism

---
+++ sunflower/ui.R
@@ -0,0 +1,22 @@
+# ui.R
+
+shinyUI(fluidPage( # function fluidPage creates a display that automatically adjusts
+  #to the dimensions of your user’s browser window
+  titlePanel("Sunflower"),
+  
+  sidebarLayout(
+    sidebarPanel( h1("Floral Heliotropism"),
+                  img(src = "sunflower.jpeg", height = 200, width = 200)),
+    mainPanel( h1("Why sunflower follows the sun?"),
+               p("Heliotropic flowers track the sun's motion across the sky from east to west.
+                 During the night, the flowers may assume a random orientation, 
+                 while at dawn they turn again toward the east where the sun rises. 
+                 The motion is performed by motor cells in a flexible segment just below the flower, 
+                 called a pulvinus. The motor cells are specialized in pumping potassium ions 
+                 into nearby tissues, changing their turgor pressure. 
+                 The segment flexes because the motor cells at the shadow side elongate due to
+                 a turgor rise. Heliotropism is a response to light from the sun. Source: wikipedia")
+    )
+  )
+))
+
